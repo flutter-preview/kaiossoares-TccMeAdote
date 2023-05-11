54f6a7bf96132fbbabb7f4d1db350f87baff05f4
guipavezzi <gui.pavezzi07@gmail.com>
2023-05-11 14:51:43 -0300

    Arrumando estrutura das pastas

---
+++ tcc_me_adote/lib/app/adoteme_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class AdoteMeApp extends StatelessWidget {
+  const AdoteMeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '',
+      routes: {
+        '/':(context) => Container(),
+      },
+    );
+  }
+}
--- tcc_me_adote/lib/main.dart
+++ tcc_me_adote/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tcc_me_adote/app/adoteme_app.dart';
 
 void main() {
-  runApp(const MyAp());
+  runApp(const AdoteMeApp());
 }
