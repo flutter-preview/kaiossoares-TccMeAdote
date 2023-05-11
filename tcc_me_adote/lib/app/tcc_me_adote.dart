ef40bfcc1e31bbab200ed8af5351648fe4fccae7
kaiossoares <kaioantoniosoares@gmail.com>
2023-05-11 17:19:12 -0300

    Mudando a Estrutura

--- tcc_me_adote/lib/app/tcc_me_adote.dart
+++ tcc_me_adote/lib/app/tcc_me_adote.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class AdoteMeApp extends StatelessWidget {
-  const AdoteMeApp({super.key});
+class TccMeAdote extends StatelessWidget {
+  const TccMeAdote({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '',
+      title: 'Me Adote',
       routes: {
         '/':(context) => Container(),
       },
--- tcc_me_adote/lib/main.dart
+++ tcc_me_adote/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tcc_me_adote/app/adoteme_app.dart';
+import 'package:tcc_me_adote/app/tcc_me_adote.dart';
 
 void main() {
-  runApp(const AdoteMeApp());
+  runApp(const TccMeAdote());
 }
