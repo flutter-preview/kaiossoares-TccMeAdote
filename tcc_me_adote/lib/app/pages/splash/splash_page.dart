69b1ec69c01ae7e8d71424f224cacbccc5880443
guipavezzi <gui.pavezzi07@gmail.com>
2023-06-07 14:03:16 -0300

    Arrumando responsividade e adicionando imagem

--- tcc_me_adote/lib/app/pages/splash/splash_page.dart
+++ tcc_me_adote/lib/app/pages/splash/splash_page.dart
@@ -14,60 +14,63 @@ class Splash extends StatelessWidget {
         ),
       ),
       child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                margin: EdgeInsets.only(right: 20, top: 20),
-                child: Align(
-                  alignment: Alignment.centerRight,
-                  child: Image.asset(
-                    '../assets/images/logo.png',
-                    width: 40,
-                    height: 40,
+        body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(right: 20),
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Image.asset(
+                        'assets/images/logo.png',
+                        width: constraints.maxWidth * 0.1,
+                        height: constraints.maxWidth * 0.1,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              SizedBox(height: 20,),
-              Image.asset(
-                '../assets/images/inicial.png',
-                width: 200,
-                height: 200,
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Container(
-                width: 335,
-                child: Text(
-                  'Compreendemos que o processo de adoção de um animal, muitas vezes não resulta como o esperado, como um animal que não está em condições para ser adotado ou um tutor que não apresenta estrutura para tal.',
-                  style: TextStyle(fontSize: 12),
-                ),
-              ),
-              SizedBox(height: 30,),
-              Container(
-                margin: EdgeInsets.only(top: 20),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/pets');
-                  },
-                  child: Text('Acessar'),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.blue),
-                    minimumSize: MaterialStateProperty.all(Size(230, 50)),
+                  SizedBox(height: constraints.maxHeight * 0.1),
+                  Image.asset(
+                    'assets/images/inicial.png',
+                    width: constraints.maxWidth * 0.7,
+                    height: constraints.maxWidth * 0.7,
                   ),
-                ),
+                  SizedBox(height: constraints.maxHeight * 0.03),
+                  Container(
+                    width: constraints.maxWidth * 0.9,
+                    child: Text(
+                      'Compreendemos que o processo de adoção de um animal, muitas vezes não resulta como o esperado, como um animal que não está em condições para ser adotado ou um tutor que não apresenta estrutura para tal.',
+                      style: TextStyle(fontSize: 13.3),
+                    ),
+                  ),
+                  SizedBox(height: constraints.maxHeight * 0.15),
+                  Container(
+                    margin: EdgeInsets.only(top: constraints.maxHeight * 0.04),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/pets');
+                      },
+                      child: Text('Acessar'),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.blue),
+                        minimumSize: MaterialStateProperty.all(Size(230, 50)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: constraints.maxHeight * 0.02),
+                  InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/rotaLogin');
+                    },
+                    child: Text('Já tem uma conta?'),
+                  ),
+                ],
               ),
-              SizedBox(height: 20),
-              InkWell(
-                onTap: () {
-                  Navigator.pushNamed(context, 'routeName');
-                },
-                child: Text('Já tem uma conta?'),
-              ),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
